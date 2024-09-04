5cdfffdfb9aa442c95627ad646dcc4a5b5c216e1
ElizarkovAV <elizarkov.av@gmail.com>
2024-09-04 13:36:36 +0300

    Update Main.java

--- src/main/java/com/elizarkov/av/Main.java
+++ src/main/java/com/elizarkov/av/Main.java
@@ -2,7 +2,7 @@ package com.elizarkov.av;
 
 public class Main {
 
-    private static String msg = "Hello qa.quru!!!!!!!";
+    private static String msg = "Hello qa.quru!";
     public static void main(String[] args) {
         System.out.println(msg);
     }
